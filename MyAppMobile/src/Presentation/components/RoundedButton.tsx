5033d58b74cc246cec7e617e9051316a3c562e70
Snnat19 <140465833+Snnat19@users.noreply.github.com>
2024-04-05 20:56:59 -0500

    component

--- MyAppMobile/src/Presentation/components/RoundedButton.tsx
+++ MyAppMobile/src/Presentation/components/RoundedButton.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { MyColors } from '../theme/AppTheme'; 
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { MyColors } from '../theme/AppTheme'
 
 interface Props {
-    text: string;
-    onPress: () => void,
-   } 
+    text: string,
+    onPress: () => void
+}
 
-   export const RoundedButton = ({ text, onPress}: Props) => {
-    return (
+export const RoundedButton = ({ text, onPress }: Props) => {
+  return (
     <TouchableOpacity
-    style={styles.RoundedButton}
-    onPress={() => onPress()}
+        style={ styles.roundedButton }
+        onPress={() => onPress()}
     >
-    <Text style={styles.textButton} >{ text}</Text>
+        <Text style={ styles.textButton }>{ text }</Text>
     </TouchableOpacity>
-    )
-   } 
+  )
+}
 
 const styles = StyleSheet.create({
-  RoundedButton: {
-    width: '100%',
-    height: 50,
-    backgroundColor: MyColors.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 15,
-  },
-  textButton: {
-    color: 'white',
-  }
+    roundedButton: {
+        width: '100%',
+        height: 50,
+        backgroundColor: MyColors.primary,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+    },
+    textButton: {
+        color: 'white',
+        // fontWeight: 'bold'
+    }
 });
